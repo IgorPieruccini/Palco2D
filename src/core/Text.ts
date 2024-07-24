22725b7fcd52c4bd7888db220c214829bab48ce3
igorpieruccini <igorpieruccini@gmail.com>
2024-07-24 23:09:41 +0200

    Calculate text boundaries

--- src/core/Text.ts
+++ src/core/Text.ts
@@ -1,34 +1,46 @@
 import { BaseEntity, BaseEntityProps } from "./BaseEntity";
 
-type TextProps = BaseEntityProps & {
-  text: string,
-  color?: string,
-  font?: string,
-  fontSize?: number,
-};
+type TextProps = Omit<BaseEntityProps, 'size'>
+  & {
+    ctx: CanvasRenderingContext2D,
+    text: string,
+    color?: string,
+    font?: string,
+    fontSize?: number,
+    maxWidth?: number
+  };
 
 export class Text extends BaseEntity {
   text: string = '';
   color: string = 'black';
   font: string = 'Arial';
   fontSize: number = 20;
+  maxWidth: number | undefined = undefined;
 
   constructor(props: TextProps) {
-    super(props);
+    const size = { x: 0, y: 0 };
+    super({ ...props, size });
 
     this.text = props.text;
 
     this.color = props.color || this.color;
     this.font = props.font || this.font;
     this.fontSize = props.fontSize || this.fontSize;
+    this.maxWidth = props.maxWidth || this.maxWidth;
   }
 
+  private setTextBoundary(ctx: CanvasRenderingContext2D) {
+    const metrics = ctx.measureText(this.text);
+    this.size = { x: metrics.width, y: metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent };
+  }
 
   render(ctx: CanvasRenderingContext2D) {
     ctx.save();
     ctx.fillStyle = this.color;
     ctx.font = `${this.fontSize}px ${this.font}`;
-    ctx.fillText(this.text, 0, 0);
+    ctx.fillText(this.text, -this.size.x / 2, this.size.y / 2, this.maxWidth);
+
+    this.setTextBoundary(ctx);
     ctx.restore();
   }
 
--- src/experiments/text-example.ts
+++ src/experiments/text-example.ts
@@ -1,3 +1,4 @@
+import { AssetHandler } from "../core/AssetHandler";
 import { Scene } from "../core/SceneHandler/Scene";
 import { Text } from "../core/Text";
 
@@ -5,19 +6,40 @@ export class TextExample extends Scene {
 
   public async start() {
 
+    await AssetHandler().loadFont('PixelifySans', 'assets/PixelifySans-Regular.ttf');
+
     const text = new Text({
-      text: 'Hello World',
+      ctx: this.ctx,
+      text: 'Click me!',
       color: 'white',
-      font: 'Arial',
+      font: 'PixelifySans',
       fontSize: 50,
-      position: { x: 100, y: 100 },
-      size: { x: 100, y: 100 },
+      position: { x: 400, y: 100 },
       rotation: 0,
+    });
+
+    text.on('mouseenter', () => {
+      text.color = 'red';
+    });
+
+    text.on('mouseleave', () => {
+      text.color = 'white';
+    });
+
+    text.on('mousedown', () => {
+      if (text.text === 'Clicked!') {
+        text.text = 'Click me!'
+        return;
+      }
+      text.text = 'Clicked!';
     });
 
 
     this.render.addEntity(text);
     this.render.startRender();
+
+    this.mouseHandler.addEntity(text);
+    this.mouseHandler.start();
   }
 
 }
