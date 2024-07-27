fb02bffcf132341aa2f924738ffa273b7dc71a9f
Igor Pieruccini <igorpieruccini@gmail.com>
2024-07-27 09:41:50 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+import { Scene } from "./src/core/SceneHandler/Scene";
+import { SceneHandler } from "./src/core/SceneHandler/SceneHandler";
+import { BatchHandler } from "./src/core/BatchHandler/BatchHandler";
+import { AssetHandler } from "./src/core/AssetHandler";
+import { BaseEntity } from "./src/core/BaseEntity";
+import { MouseHandler } from "./src/core/MouseHandler";
+import { RenderHandler } from "./src/core/RenderHandler";
+import { Sprite } from "./src/core/Sprite";
+import { Text } from "./src/core/Text";
+
+export const Palco2D = {
+  Scene,
+  SceneHandler,
+  BatchHandler,
+  AssetHandler,
+  BaseEntity,
+  MouseHandler,
+  RenderHandler,
+  Sprite,
+  Text,
+};
+
