acf5cc674e92b8716fb463bcfa985267a723088b
Igor Pieruccini <igorpieruccini@gmail.com>
2025-03-09 14:17:13 +0100

    Add docusaurus basic setup

---
+++ website/src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,10 @@
+import type { ReactNode } from "react";
+import styles from "./styles.module.css";
+
+export default function HomepageFeatures(): ReactNode {
+  return (
+    <section className={`${styles.container}`}>
+      <h2 className="center-text">Documentation under construction</h2>
+    </section>
+  );
+}
