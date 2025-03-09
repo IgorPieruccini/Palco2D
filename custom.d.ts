ee2f878b942a279b431e38949ec1d5905ce26f91
Igor Pieruccini <igorpieruccini@gmail.com>
2025-03-09 14:28:22 +0100

    Deploy website - based on b29d3e5d1b250f9b14454d3c6b239f1e1a08d430

--- custom.d.ts
+++
@@ -1,3 +0,0 @@
-interface Window {
-  gl: WebGL2RenderingContext;
-}
--- packages/plugins/index.ts
+++
@@ -1,3 +0,0 @@
-export { FPSViewPlugin } from "./src/FPSViewPlugin";
-export { InfinityCanvasPlugin } from "./src/InfinityCanvasPlugin";
-export { TransformerEntityController } from "./src/TransformerEntityPlugin/TransformerEntityPlugin";
